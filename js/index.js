7e0d5877cba99556c9deafa959e89f24a4f501b2
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-12-27 00:02:52 +0530

    form added

--- js/index.js
+++ js/index.js
@@ -5,6 +5,18 @@ var typed = new Typed('#element', {
   typeSpeed: 50,
   loop:true,
   fadeout:true,
-  fadeOutClass: 'typed-fade-out',
-  
+  fadeOutClass: 'typed-fade-out',  
+});
+
+
+//form event handlers
+//showing the prevoe of the forms
+let inputBox =  document.getElementsByTagName('input');
+Array.from(inputBox).forEach(e=>{
+e.addEventListener('keyup',(option)=>{
+if(option.target.id == 'url')
+{
+    document.getElementsByClassName(option.target.id)[0].innerText = e.value ;
+}
+})
 });
