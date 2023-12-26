d9e80677ed17cb1953b11895d6dc626c9ed9133b
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-12-26 23:09:05 +0530

    Added Form

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//type js
+
+var typed = new Typed('#element', {
+  strings: ['The best code is no code at all', 'Good code is its own best documentation','Code never lies, comments sometimes do'],
+  typeSpeed: 50,
+  loop:true,
+  fadeout:true,
+  fadeOutClass: 'typed-fade-out',
+  
+});
